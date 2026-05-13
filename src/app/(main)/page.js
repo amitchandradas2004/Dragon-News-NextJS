56593b1eef84447d3484ae0a0a7e0e16be8794a7
amitchandradas2004 <amitcdas792@gmail.com>
2026-05-13 14:45:24 +0600

    All Categories are added dynamically from API

--- src/app/(main)/page.js
+++ src/app/(main)/page.js
@@ -1,9 +1,34 @@
-import Image from "next/image";
+async function getCategories() {
+  const res = await fetch(
+    "https://openapi.programming-hero.com/api/news/categories",
+  );
+  const data = await res.json();
+  const category = data.data.news_category;
+  return category;
+}
+export default async function Home() {
+  const categories = await getCategories();
+  console.log(categories);
 
-export default function Home() {
   return (
-    <div className="bg-green-200 h-screen text-6xl md:text-7xl lg:text-8xl font-bold flex justify-center items-center">
-      <h2>This is the Home Page</h2>
+    <div className="grid grid-cols-12 container mx-auto gap-4 my-5">
+      <div className="col-span-3">
+        <h2 className="font-bold text-xl text-center">All Categories</h2>
+        <ul className="flex flex-col gap-2 mt-5">
+          {categories.map((category) => (
+            <button
+              key={category.category_id}
+              className="p-2 rounded-full font-medium text-center btn"
+            >
+              {category.category_name}{" "}
+            </button>
+          ))}
+        </ul>
+      </div>
+      <div className="font-bold text-xl bg-blue-200 col-span-6">All News</div>
+      <div className="font-bold text-xl bg-purple-200 col-span-3">
+        Social Icons
+      </div>
     </div>
   );
 }
---
+++ src/app/loading.jsx
@@ -0,0 +1,9 @@
+const loading = () => {
+  return (
+    <div className="h-screen bg-gray-700">
+      <span className="loading loading-spinner text-primary w-50"> </span>
+    </div>
+  );
+};
+
+export default loading;
