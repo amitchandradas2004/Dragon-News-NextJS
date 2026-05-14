bfae76275fd4109834d864831853ba618db10454
amitchandradas2004 <amitcdas792@gmail.com>
2026-05-14 09:13:09 +0600

    Login page design is done

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,9 +1,122 @@
-export const metadata = {
-  title: "Login Page",
-  description: "This is the Login Page",
-};
-const LoginPage = () => {
-  return <div>This is the login Page</div>;
-};
+"use client";
 
-export default LoginPage;
+import { Check, Eye, EyeSlash } from "@gravity-ui/icons";
+import {
+  Button,
+  Description,
+  FieldError,
+  Form,
+  Input,
+  InputGroup,
+  Label,
+  TextField,
+} from "@heroui/react";
+import Link from "next/link";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+
+export default function LoginPage() {
+  const [isVisible, setIsVisible] = useState(false);
+  const { register, handleSubmit } = useForm();
+  const handleLoginFunction = (data) => {
+    console.log(data, "data");
+
+    // const formData = new FormData(e.currentTarget);
+    // const data = {};
+    // // Convert FormData to plain object
+    // formData.forEach((value, key) => {
+    //   data[key] = value.toString();
+    // });
+    // alert(`Form submitted with: ${JSON.stringify(data, null, 2)}`);
+  };
+
+  return (
+    <div className="container mx-auto mt-5">
+      {" "}
+      <Form
+        className="flex flex-col w-100 p-5 rounded-2xl bg-black/5 mx-auto gap-4 border border-slate-300 justify-center shadow-xl"
+        onSubmit={handleSubmit(handleLoginFunction)}
+      >
+        <h2 className="font-bold text-center text-xl">Login your account</h2>
+        <TextField
+          isRequired
+          type="email"
+          validate={(value) => {
+            if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(value)) {
+              return "Please enter a valid email address";
+            }
+
+            return null;
+          }}
+        >
+          <Label>Email address</Label>
+          <InputGroup className="rounded-full">
+            <InputGroup.Input
+              {...register("email")}
+              placeholder="Enter your email address"
+              className="rounded-full w-full"
+            />
+          </InputGroup>
+          <FieldError />
+        </TextField>
+
+        <TextField
+          isRequired
+          className="w-full rounded-full"
+          validate={(value) => {
+            if (value.length < 8) {
+              return "Password must be at least 8 characters";
+            }
+            if (!/[A-Z]/.test(value)) {
+              return "Password must contain at least one uppercase letter";
+            }
+            if (!/[0-9]/.test(value)) {
+              return "Password must contain at least one number";
+            }
+
+            return null;
+          }}
+        >
+          <Label>Password</Label>
+          <InputGroup className="rounded-full">
+            <InputGroup.Input
+              {...register("password")}
+              placeholder="Enter your password"
+              className="w-full rounded-full"
+              type={isVisible ? "text" : "password"}
+            />
+            <InputGroup.Suffix className="pr-0">
+              <Button
+                isIconOnly
+                aria-label={isVisible ? "Hide password" : "Show password"}
+                size="sm"
+                variant="ghost"
+                onPress={() => setIsVisible(!isVisible)}
+              >
+                {isVisible ? (
+                  <Eye className="size-4" />
+                ) : (
+                  <EyeSlash className="size-4" />
+                )}
+              </Button>
+            </InputGroup.Suffix>
+          </InputGroup>
+          <FieldError />
+        </TextField>
+
+        <div className="flex flex-col gap-2 w-full text-center">
+          <Button type="submit" className="w-full">
+            <Check />
+            Login
+          </Button>
+          <span className="font-medium">
+            Dont’t Have An Account ?{" "}
+            <Link href="/register">
+              <span className="text-red-600">Register</span>
+            </Link>
+          </span>
+        </div>
+      </Form>
+    </div>
+  );
+}
